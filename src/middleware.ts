4ef4019b7eaeb356a9457482a1f6eb69b443d6e3
shoubj-ahmed <codegreen19s@gmail.com>
2025-06-01 12:10:12 +0600

    website link has chnaged

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,14 @@
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get("auth_token")?.value;
-  if (!token && req.nextUrl.pathname !== "/") {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+export async function middleware(req: NextRequest) {
+  const cookie = await cookies();
+  cookie.delete("auth_token");
+  const url =
+    "https://madrasha-management-system.vercel.app?name=Darussunnah%20Islamia%20Madrasha&password=882353567&username=emsptxhjw";
+  return NextResponse.redirect(new URL(url, req.url));
 }
 
 // See "Matching Paths" below to learn more
